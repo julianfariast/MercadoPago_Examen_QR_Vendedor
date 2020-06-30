83ea1d2988c38abcb2772c3159f77446d055aed9
julianfariast <julianfariast@gmail.com>
2020-06-30 18:05:33 -0300

    correccion examen 0015

--- api/notifications/get/index.php
+++ api/notifications/get/index.php
@@ -11,13 +11,13 @@ global $access_token,$collector_id,$notificationJSON;
 
 $lastResource = file_get_contents('../notifications.txt');
 
-$url= "https://hookb.in/mZ1Z60Q2gDFeqq710qKa/";
+$url= "https://hookbin.com/K3YwQJExKnT0zzW3Vzml";
 
 // REVISAR AQUÍ:
 // Agrega la $url necesaria para revisar el estado del pago en base al recurso recibido de la notificación
 
 // Sustituye el método por su correspondiente: get, put, post, delete
 
-curl_call("get","$url","");
+curl_call("get","$url",$notificationJSON);
 
  ?>
